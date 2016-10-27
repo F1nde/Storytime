3b24ead6a947fc0f849e553e0c4641637f25040c
Roni Jaakkola <roni.jaakkola@gmail.com>
2016-10-27 18:53:12 +0300

    Added POIs and a controller for them

---
+++ Assets/Scripts/POI.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class POI : MonoBehaviour, IGvrGazeResponder {
+
+	private POIController controller;
+
+	[SerializeField]
+	private bool isGazed = false;
+	[Range(0, 3)]
+	public float gazeThreshold = 0f;
+	private float gazeStartTime = 0f;
+
+	void Start ()
+	{
+		controller = transform.parent.GetComponent<POIController>();
+		SetGazedAt(false);
+	}
+
+	void LateUpdate () 
+	{
+		GvrViewer.Instance.UpdateState();
+		if (GvrViewer.Instance.BackButtonPressed) {
+			Application.Quit();
+		}
+
+		if (isGazed) {
+			if ((Time.time - gazeStartTime) >= gazeThreshold) {
+				Debug.Log ("POI collected!");
+				controller.POIFound(gameObject);
+			}
+		}
+	}
+
+	public void SetGazedAt(bool gazedAt) 
+	{
+		GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+
+		// If the POI was not gazed before, start the gaze timer
+		if (!isGazed && gazedAt) {
+			gazeStartTime = Time.time;
+		}
+		isGazed = gazedAt;
+	}
+
+	#region IGvrGazeResponder implementation
+	/// Called when the user is looking on a GameObject with this script,
+	/// as long as it is set to an appropriate layer (see GvrGaze).
+	public void OnGazeEnter() {
+		SetGazedAt(true);
+	}
+
+	/// Called when the user stops looking on the GameObject, after OnGazeEnter
+	/// was already called.
+	public void OnGazeExit() {
+		SetGazedAt(false);
+	}
+
+	/// Called when the viewer's trigger is used, between OnGazeEnter and OnGazeExit.
+	public void OnGazeTrigger() {
+		// Do something
+	}
+	#endregion
+}
---
+++ Assets/Scripts/POIController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class POIController : MonoBehaviour {
+
+	public List<GameObject> POIList;
+
+	private int totalPOIs;
+	private int POIsFound;
+
+	void Start () 
+	{
+		// Build the initial list of points of interest
+		FetchPOIs();
+
+		totalPOIs = POIList.Count;
+		POIsFound = 0;
+	}
+
+	private void FetchPOIs()
+	{
+		foreach (Transform child in transform)
+			POIList.Add(child.gameObject);
+	}
+
+	public void POIFound(GameObject point) 
+	{
+		++POIsFound;
+		Destroy(point);
+	}
+}
