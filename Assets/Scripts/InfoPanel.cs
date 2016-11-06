ef1684c6537a02a5097a4f1c9da94b2e249f5f92
Marko Tuominen <tuominen@spinthedisc.net>
2016-11-06 15:07:08 +0200

    Added status canvas to show current POI findings

---
+++ Assets/Scripts/InfoPanel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class InfoPanel : MonoBehaviour
+{
+    public Camera cam;
+
+    private GameObject POIController;
+    private Text textField;
+
+    void Awake()
+    {
+        textField = GetComponent<Text>();
+    }
+
+    void Start()
+    {
+        // Tie panel to main camera
+        if (cam == null)
+        {
+            cam = Camera.main;
+            transform.SetParent(cam.GetComponent<Transform>(), true);
+        }
+
+        // Find POIController for status information
+        POIController = GameObject.Find("POIController");
+    }
+
+    void LateUpdate()
+    {
+        List<int> status = POIController.GetComponent<POIController>().POIStatus();
+
+        if(status.Count == 2)
+            textField.text = "POIs found " + status[0] + " / " + status[1];
+    }
+}
--- Assets/Scripts/POIController.cs
+++ Assets/Scripts/POIController.cs
@@ -6,7 +6,7 @@ public class POIController : MonoBehaviour {
 
 	public List<GameObject> POIList;
 
-	private int totalPOIs;
+    private int totalPOIs;
 	private int POIsFound;
 
 	void Start () 
@@ -30,8 +30,21 @@ public class POIController : MonoBehaviour {
         point.SetActive(false);
 	}
 
+    // Returns current status inside list
+    public List<int> POIStatus()
+    {
+        List<int> status = new List<int>();
+
+        status.Add(POIsFound);
+        status.Add(totalPOIs);
+
+        return status;
+    }
+
     public void POIReset()
     {
+        POIsFound = 0;
+
         for (int i = 0; i < POIList.Count; ++i)
         {
             if (POIList[i].activeSelf == false)
